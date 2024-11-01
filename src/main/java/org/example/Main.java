a8ae03f35272bce79811917b2fe93f1c84412734
unknown <ozmanmail14@gmail.com>
2024-11-01 11:55:15 +0000

    Monster class created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.example.Model.Monster;
+
 public class Main {
     public static void main(String[] args) {
 
+        Monster Monster1 = new Monster(1,1,"M1");
+        Monster Monster2 = new Monster(2,2,"M2");
+        Monster Monster3 = new Monster(3,3,"M3");
 
 
     }
---
+++ src/main/java/org/example/Model/Monster.java
@@ -0,0 +1,18 @@
+package org.example.Model;
+
+public class Monster extends Character {
+
+    public Monster(int x, int y, String token) {
+        super(x, y, token);
+    }
+
+    public void Greeting (){
+        System.out.println(Greeting);
+    }
+
+    String Greeting = "You have been caught";
+
+    
+
+
+}
